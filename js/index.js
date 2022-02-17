8c7ecf009a7207ef91642f65eedfd430d416b55b
neverswood <nevers_wood@hotmail.fr>
2022-02-17 14:43:58 +0100

    Display photographers items

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
+import HomePage from "./homePage/homePage.js";
+
 const main = async () => {
   const response = await fetch("../api/FishEyeData.json");
   const data = await response.json();
 
   if (window.location.pathname.includes("/photographer.html")) {
   } else {
+    const homePage = new HomePage(data);
+    document.getElementById("photographers").innerHTML =
+      homePage.displayPhotographerItem();
   }
 };
 
