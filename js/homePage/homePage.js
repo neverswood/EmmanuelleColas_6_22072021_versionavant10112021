2e72beab601580a6fee3fa23d1cd3da1521d9dc6
neverswood <nevers_wood@hotmail.fr>
2022-02-28 17:05:53 +0100

    Bind keyboard event listeners homepage

--- js/homePage/homePage.js
+++ js/homePage/homePage.js
@@ -9,11 +9,28 @@ export default class HomePage {
     });
     this.returnMain = new ReturnMain();
     this.barTags = new BarTags(data);
+    document.getElementById("photographers").innerHTML =
+      this.renderPhotographerItems();
+    this.bindKeyboardEventListeners();
   }
 
-  displayPhotographerItem() {
+  renderPhotographerItems() {
     return `${this.photographerItems
       .map((photographerItem) => photographerItem.renderPhotographerItem())
       .join("")}`;
+  }
+
+  bindKeyboardEventListeners() {
+    window.addEventListener("keydown", (e) => {
+      if (e.target.matches("#header-navbar > a")) {
+        const filterName = e.target
+          .querySelector(".btnTags")
+          .getAttribute("data-filter");
+        const { key } = e;
+        if (key === "Enter") {
+          this.barTags.filter(filterName);
+        }
+      }
+    });
   }
 }
